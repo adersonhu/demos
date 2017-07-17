3f9145eeb89c14d8d8034a5f24f8359a386f729f
adersonhu <adersonhu@163.com>
2017-07-17 14:50:47 +0800

    project create by eclispe
    the first submit,create project

---
+++ src/main/java/com/aderson/hadoop/demos/App.java
@@ -0,0 +1,13 @@
+package com.aderson.hadoop.demos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
